96ce35ed41f6569030814860c466a654d8a80ac4
sakibssamad <sssakib.info@gmail.com>
2021-08-17 11:24:16 +0600

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/flutter_name_route_navigation/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_name_route_navigation
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp( MaterialApp(
+    title: 'Named Route Navigation',
+    theme: ThemeData(
+      // This is the theme of your application.
+      primarySwatch: Colors.green,
+    ),
+    // Start the app with the "/" named route. In this case, the app starts
+    // on the FirstScreen widget.
+    initialRoute: '/',
+    routes: {
+      // When navigating to the "/" route, build the FirstScreen widget.
+      '/': (context) => HomeScreen(),
+      // When navigating to the "/second" route, build the SecondScreen widget.
+      '/second': (context) => SecondScreen(),
+    },
+  ));
+}
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home Screen'),
+      ),
+      body: Center(
+        child: RaisedButton(
+          child: Text('Click Here'),
+          color: Colors.orangeAccent,
+          onPressed: () {
+            Navigator.pushNamed(context, '/second');
+          },
+        ),
+      ),
+    );
+  }
+}
+
+class SecondScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Second Screen"),
+      ),
+      body: Center(
+        child: RaisedButton(
+          color: Colors.blueGrey,
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          child: Text('Go back!'),
+        ),
+      ),
+    );
+  }
+}
